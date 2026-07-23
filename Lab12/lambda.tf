1f94c956ba677574575ba245d7c5bdfdde98b3b5
Daniel C <daniel@Daniels-MacBook-Pro.local>
2026-07-22 20:00:24 -0400

    add schedulers

--- Lab12/lambda.tf
+++ Lab12/lambda.tf
@@ -48,3 +48,42 @@ resource "aws_iam_policy" "filter_waf_logs_policy" {
     ]
   })
 }
+
+resource "aws_iam_role" "eventbridge_scheduler_role" {
+  name = "eventbridge-scheduler-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "scheduler.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "scheduler_lambda_policy" {
+  name = "scheduler-lambda-policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "lambda:InvokeFunction"
+        ]
+        Resource = [aws_lambda_function.executive_dashboard_agent_lambda.arn, aws_lambda_function.waf_threat_correlation_lambda.arn, aws_lambda_function.waf_analyzer_lambda.arn]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "schedule_role_attachment_lambda" {
+  role       = aws_iam_role.eventbridge_scheduler_role.name
+  policy_arn = aws_iam_policy.scheduler_lambda_policy.arn
+}
